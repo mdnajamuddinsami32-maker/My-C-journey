7840f48e01c143730b3e9af7263efb095283b890
mdnajamuddinsami32-maker <mdnajamuddinsami32@gmail.com>
2026-09-26 17:32:44 +0600

    up

---
+++ C journey 2/problem2.c
@@ -0,0 +1,12 @@
+// Problem 2: a = 15, b = 25, c = 10.     a + b + c কত?
+
+#include <stdio.h>
+
+int main(){
+    int a=15;
+    int b=25;
+    int c=10;
+    printf("Your answer is: %d", a+b+c);
+
+    return 0;
+}
