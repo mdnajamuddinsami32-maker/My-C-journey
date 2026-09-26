cd0cc6b7fc609bf531777658aecc1ee359618c6d
mdnajamuddinsami32-maker <mdnajamuddinsami32@gmail.com>
2026-09-26 12:28:24 +0600

    update

--- sami.c
+++ sami.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hey Sami, congratulations! You can successfully compile and run a c program. \n");
+    printf("Hey Sami, congratulations! You can successfully compile and run a c program.\n");
+    // এখানে যদি আমরা retunrn 0 দেই, তাহলে প্রথম printf function এর প্রোগ্রাম রান হয়ে প্রোগ্রাম শেষ হয়ে যাবে। তাই আমরা return 0; এখানে ব্যবহার করব না । 
+    // এখানে আমরা \n ব্যবহার করেছি, যা একটি newline character। এটি আমাদের প্রোগ্রামের আউটপুটকে পরবর্তী লাইনে নিয়ে যাবে।
+    printf("This is Sami, a new programmer who is learning c programming language.");
     return 0;
+    // এখানে আমরা return 0; ব্যবহার করেছি, যা আমাদের প্রোগ্রামের সফল সমাপ্তি নির্দেশ করে।
 }
