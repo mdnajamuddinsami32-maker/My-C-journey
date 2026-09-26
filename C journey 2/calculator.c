b38562613d225ef774cddd2d3cc39ccc170c8f05
mdnajamuddinsami32-maker <mdnajamuddinsami32@gmail.com>
2026-09-26 13:07:46 +0600

    up

--- C journey 2/calculator.c
+++ C journey 2/calculator.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main() {
- int a, b;
-printf(" Enter a number:\n");
-scanf("%d", &a);
-printf("Enter another number:\n");
-scanf("%d", &b);
-printf("The sum is %d\n", a+b);
-return 0;
+    int a, b;
+
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    printf("Enter another number: ");
+    scanf("%d", &b);
+
+    printf("The sum is %d\n", a + b);
+
+    return 0;
 }
