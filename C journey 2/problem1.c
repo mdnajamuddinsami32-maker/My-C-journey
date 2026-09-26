c0bbae49754969bdfd95bb02331f5621fa181acd
mdnajamuddinsami32-maker <mdnajamuddinsami32@gmail.com>
2026-09-26 17:25:18 +0600

    up

---
+++ C journey 2/problem1.c
@@ -0,0 +1,16 @@
+//Problem 1 : a = 20, b = 8 .      a + b কত?
+#include <stdio.h>
+
+int main() {
+    int a,b;
+
+    printf("একটা সংখ্যা লিখো");
+    scanf("%d", &a);
+
+    printf("আরেকটা সংখ্যা লিখো");
+    scanf("%d", &b);
+
+    printf("তোমার সংখ্যা দুটির যোগফল হলোঃ %d", a+b);
+
+    return 0;
+}
