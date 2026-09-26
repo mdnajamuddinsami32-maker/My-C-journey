95636d172ac2ff9b2df3e61e49252ccb4166efe2
mdnajamuddinsami32-maker <mdnajamuddinsami32@gmail.com>
2026-09-26 17:30:08 +0600

    উপ

--- C journey 2/problem1.c
+++ C journey 2/problem1.c
@@ -1,16 +1,10 @@
-//Problem 1 : a = 20, b = 8 .      a + b কত?
+//Problem 1 : a = 20, b = 8 .  a + b কত?
 #include <stdio.h>
-
 int main() {
-    int a,b;
-
-    printf("একটা সংখ্যা লিখো");
-    scanf("%d", &a);
-
-    printf("আরেকটা সংখ্যা লিখো");
-    scanf("%d", &b);
-
-    printf("তোমার সংখ্যা দুটির যোগফল হলোঃ %d", a+b);
+    int a=20;
+    int b=8;
+    printf("The sum is %d\n", a + b);
 
     return 0;
-}
+} 
+// বিয়োগ এর ক্ষেত্রে শুধু a - b করতে হবে।
